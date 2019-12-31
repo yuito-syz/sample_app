f9044752737de02fda082c096d9474352e555c16
EC2 Default User <ec2-user@ip-172-31-34-188.ap-northeast-1.compute.internal>
2019-12-31 08:04:10 +0000

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless @user.activated?
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
@@ -53,15 +53,6 @@ class UsersController < ApplicationController
    end
    
    # beforeアクション
-   
-   # ログイン済ユーザーかどうか確認
-   def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:denger] = "Please log in."
-      redirect_to login_url
-    end
-   end
    
    # 正しいユーザーかどうか確認
    def correct_user
